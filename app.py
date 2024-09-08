37ca8fdada8c4a4c92e75db78ecc288e6a76e819
Christopher Wangsanata <chriswangsanata@gmail.com>
2024-09-07 22:45:14 -0700

    implement requests

--- app.py
+++ app.py
@@ -1,28 +1,34 @@
 import requests
 import os
 import yaml
-import logging
-from util.connector import Connector
-
-CLIENT_NAME="roku_tv_main_bedroom"
-IP="192.168.0.14"
-PORT=8060
+from util import select_random
+# import logging
 
 def main():
     # Load configuration from YAML file
-    with open('/config/settings.yaml', 'r') as file:
+    with open('config/settings.yaml', 'r') as file:
         config = yaml.safe_load(file)
 
-    # Use environment variables if available, otherwise use config file
     device_ip = os.getenv('DEVICE_IP', config['device']['ip'])
     device_port = os.getenv('DEVICE_PORT', config['device']['port'])
     device_timeout = config['device']['timeout']
+    
+    video_ids = config['content']['video_ids']
 
-    # TODO: Change to logging
-    print(f"Connecting to device at {device_ip}:{device_port} with a timeout of {device_timeout} seconds.")
+    # TODO: Change all prints to logging and print too
+    print(f"Attempting to connect to device at {device_ip}:{device_port} with a timeout of {device_timeout} seconds.")
+    
+    # TODO: Add logging here for connection
+    try:
+        url = f"http://{device_ip}:{device_port}/launch/837?contentId={video_ids[select_random(video_ids)]}"
+        response = requests.post(url=url, timeout=device_timeout)
+    except ConnectionError as e:
+        print(e)
 
-    # Send the request
-    r = requests()
+    print(response)
+
+if __name__=='__main__':
+    main()
 
 
     
