5775915f7c1bdf7c286639d7606e2646d4b92d9c
Christopher Wangsanata <chriswangsanata@gmail.com>
2024-09-10 02:03:45 -0700

    util cleanup

--- util.py
+++ util.py
@@ -1,8 +1,8 @@
 import random
 import yaml
 
-def select_random(arr) -> int:
-    return random.randrange(0, len(arr))
+def select_random(arr: list) -> int:
+    return arr[random.randrange(0, len(arr))]
 
 def load_yaml(file_path: str) -> any:
     with open(file_path, 'r') as file:
