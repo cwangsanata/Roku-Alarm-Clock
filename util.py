bdb408642a78390a986205f5212511faae89e152
Christopher Wangsanata <chriswangsanata@gmail.com>
2024-09-10 01:10:00 -0700

    move file parsing

--- util.py
+++ util.py
@@ -1,4 +1,10 @@
 import random
+import yaml
 
-def select_random(arr):
+def select_random(arr) -> int:
     return random.randrange(0, len(arr))
+
+def load_yaml(file_path: str) -> any:
+    with open(file_path, 'r') as file:
+        return yaml.safe_load(file)
+
