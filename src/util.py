a04d528a79593967f8d41ff5f99b631add310097
Chris Wangsanata <chriswangsanata@gmail.com>
2024-09-25 22:56:05 -0700

    add assertions

--- src/util.py
+++ src/util.py
@@ -2,9 +2,13 @@ import random
 import yaml
 
 def select_random(arr: list) -> int:
-    return arr[random.randrange(0, len(arr))]
+    try:
+        return arr[random.randrange(0, len(arr))]
+    except ValueError:
+        raise IndexError('Cannot select random element from empty list')
 
 def load_yaml(file_path: str) -> any:
     with open(file_path, 'r') as file:
+        assert file, f'File {file_path} not found'
         return yaml.safe_load(file)
 
