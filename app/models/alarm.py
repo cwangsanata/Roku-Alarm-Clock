1fbc3cb2de468d44c7a21557880524e2956d8a45
Chris Wangsanata <chriswangsanata@gmail.com>
2024-10-09 00:02:26 -0700

    update local column name

--- app/models/alarm.py
+++ app/models/alarm.py
@@ -5,7 +5,7 @@ class Alarm(db.Model):
     
     id = db.Column(db.Integer, primary_key=True) 
     day = db.Column(db.String(10), nullable=False) 
-    alarm_time_id = db.Column(db.Integer, db.ForeignKey('alarm_times.id'), nullable=False)  # Foreign key to Alarm_Times
+    time_id = db.Column(db.Integer, db.ForeignKey('times.id'), nullable=False)  # Foreign key to Alarm_Times
     video_id = db.Column(db.Integer, db.ForeignKey('videos.id'), nullable=False)  # Foreign key to Videos
     note = db.Column(db.String, nullable=True) 
 
