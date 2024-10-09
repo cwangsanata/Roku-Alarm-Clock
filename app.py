994339b3d047fbe955f05172cc4f109d0c32efe8
Chris Wangsanata <chriswangsanata@gmail.com>
2024-10-08 22:38:19 -0700

    refactor, rename, and reorganize files

--- app.py
+++ app.py
@@ -2,14 +2,25 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+### ERROR HANDLERS ###
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template('index.html'), 404
 
+### INDEX ROUTE ###
 @app.route('/')
 def index():
     return render_template('index.html')
 
+### DEVICE ROUTES ###
+@app.route('/devices', methods=['GET', 'POST'])
+def devices():
+    if request.method == 'GET':
+        return "<p>GET, Devices!</p>"
+    if request.method == 'POST':
+        return "<p>POST, Devices!</p>"
+
+### ALARM ROUTES ###
 @app.route('/alarms', methods=['GET', 'POST'])
 def alarms():
     if request.method == 'GET':
@@ -17,6 +28,7 @@ def alarms():
     if request.method == 'POST':
         return "<p>POST, Alarms!</p>"
 
+### VIDEO ROUTES ###
 @app.route('/videos', methods=['GET', 'POST'])
 def videos():
     if request.method == 'GET':
